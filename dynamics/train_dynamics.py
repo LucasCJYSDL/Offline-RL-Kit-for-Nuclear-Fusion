4f153d067618a1d0ae10c6b558a0523785cac6c2
nnvvff <884473883@qq.com>
2026-05-04 00:46:15 +0800

    reconstruct dynamic model & data processing

--- dynamics/train_dynamics.py
+++ dynamics/train_dynamics.py
@@ -1,151 +1,92 @@
-import sys, os
-os.environ["HYDRA_FULL_ERROR"] = "1"
+import os
 
-import warnings
-warnings.filterwarnings("ignore")
-
-import ray, time
-import hydra, pickle
-import numpy as np
-from offlinerlkit.paths import dynamics_root
-from dynamics.utils import get_EYX, get_t_confidence_interval, get_B_star
+import hydra
+from hydra.utils import get_original_cwd
 from omegaconf import DictConfig, OmegaConf, open_dict
 from pytorch_lightning.utilities.seed import seed_everything
-from dynamics_toolbox.utils.lightning.constructors import construct_all_pl_components_for_training
 
+import dynamics_toolbox
+from dynamics_toolbox.utils.lightning.constructors import (
+    construct_all_pl_components_for_training,
+)
 
-@ray.remote(num_gpus=1)  # Request 1/6 of a GPU
-def train_single_model(cfg: DictConfig, ensemble_id: int, exp_id: int, B_star: int, num_models_so_far: int):
-    save_dir = os.path.join(str(dynamics_root), "exp_{}".format(exp_id)) # DANGER: could take a lot of disk space.
-    # Clone the config for this ensemble member
-    cfg = OmegaConf.create(OmegaConf.to_container(cfg, resolve=True))
+from dynamics_toolbox.utils.storage.model_storage import (
+    load_model_from_log_dir,
+    load_ensemble_from_parent_dir,
+)
+
+import fusion_control
+import os
+os.environ["HYDRA_FULL_ERROR"] = "1"
+
+@hydra.main(
+    #config_path=f'{os.environ["FUSION_CONTROL_PROJECT_PATH"]}/cfgs/dynamics',
+    config_path=f'{os.path.dirname(__file__)}/cfgs',
+    # config_name='pnn')
+    # config_name='profile_rnn_gpu')
+    # config_name='profile_prnn_gpu')
+    # config_name='profile_mlp_gpu')
+    # config_name='profile_simplex_gpu')
+    # config_name="rpnn_noshape_gas_transfer_step_two_nll",
+    # config_name="tpnn_noshape_gas_bms_nll",
+    #config_name="rpnn_noshape_gas_benchmark1",
+    config_name="rpnn_noshape_gas_benchmark1",
+)
+def train(cfg: DictConfig) -> None:
+    """Train the model."""
+    if "model" not in cfg:
+        raise ValueError(
+            "model must be specified. Choose one of the provided "
+            "model configurations and set +model=<model_config>."
+        )
+    if "seed" in cfg:
+        seed_everything(cfg["seed"])
+    # Alter config file and add defaults.
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg["cuda_device"])
     with open_dict(cfg):
-        # Update seed & save directory for uniqueness
-        cfg["data_module"]["B_star"] = B_star # hulc
-        cfg["data_module"]["ensemble_id"] = ensemble_id # hulc
-        cfg["data_module"]["seed"] = cfg["seed"] + num_models_so_far # all ensemble members share the same seed for the data module?
-        cfg["seed"] = cfg["seed"] + num_models_so_far + ensemble_id
-        # cfg["data_module"]["seed"] = cfg["seed"] # TODO
-        cfg["data_module"]["save_dir"] = os.path.join(save_dir, str(cfg["seed"])) # save_dir for data
-        cfg["save_dir"] = os.path.join(save_dir, str(cfg["seed"]), "model") # save_dir for model
-        cfg["save_path"] = os.path.join(str(dynamics_root), cfg.get("experiment_name", f"exp_{exp_id}"))
+        if "save_dir" not in cfg:
+            cfg["save_dir"] = os.path.join(os.getcwd(), "model")
+        elif cfg["save_dir"][0] != "/":
+            cfg["save_dir"] = os.path.join(get_original_cwd(), cfg["save_dir"])
+        if "gpus" in cfg:
+            cfg["gpus"] = str(cfg["gpus"])
+    model, data, trainer, logger, cfg = construct_all_pl_components_for_training(cfg)
+    #load model if load_checkpoint is given
 
-    # you should either comment the following line or assign random seeds (i.e., cfg["seed"]) based on the exp_id
-    # to ensure that the training result of each exp is different
-    seed_everything(cfg["seed"])
+    # print("\n\n$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n")
+    # print("Load checkpoint = ",cfg.get('load_checkpoint',None))
+    # if cfg.get('load_checkpoint',None) is not None:
+    #     # model = model.load_from_checkpoint(cfg['load_checkpoint'])
+    #     print("\n\nLoading model from checkpoint - SEED  = ", cfg['seed'],"\n\n")
+    #     load_checkpoint = cfg['load_checkpoint']+f"/{cfg['seed']}"
+    #     model = load_model_from_log_dir(load_checkpoint)
 
-    # get all components
-    model, data, trainer, logger, cfg = construct_all_pl_components_for_training(cfg) # logger.savedir is the same as cfg["save_dir"]
-    # print(OmegaConf.to_yaml(cfg))
-    save_path = os.path.join(save_dir, str(cfg["seed"]))
-    os.makedirs(save_path, exist_ok=True)
+    print(OmegaConf.to_yaml(cfg))
+    if cfg["logger"] == "mlflow":
+        save_path = os.path.join(cfg["save_dir"], logger.experiment_id, logger.run_id)
+    else:
+        if "run_name" in cfg:
+            name = os.path.join(cfg["experiment_name"], cfg["run_name"])
+        else:
+            name = cfg["experiment_name"]
+        save_path = os.path.join(logger.save_dir, name, f"version_{logger.version}")
+        save_path = os.getcwd()
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     OmegaConf.save(cfg, os.path.join(save_path, "config.yaml"))
-
-    # start the training process
     trainer.fit(model, data)
-
-    # return the test_shots and ensemble_dir, which are shared by all ensemble members
-    test_shots, ensemble_dir = None, None
-    if ensemble_id == 0:
-        test_shots = data._te_dataset.tensors # a little buggy
-        ensemble_dir = save_dir
-
-    # evluations
     if data.test_dataloader() is not None:
         test_dict = trainer.test(model, datamodule=data, ckpt_path="best")[0]
-        with open(os.path.join(save_path, "test_results.txt"), "w") as f:
+        with open("test_results.txt", "w") as f:
             f.write("\n".join([f"{k}: {v}" for k, v in test_dict.items()]))
         tune_metric = cfg.get("tune_metric", "test/loss")
         return_val = test_dict[tune_metric]
         if cfg.get("tune_objective", "minimize") == "maximize":
             return_val *= -1
-        return return_val, test_shots, ensemble_dir
+        return return_val
     else:
-        return 0, test_shots, ensemble_dir
-
-
-@hydra.main(
-    config_path=f'{os.path.dirname(__file__)}/cfgs',
-    config_name="rpnn_noshape_ech" #!!! what you need to specify
-) # load arguments from the config file
-
-def train_ensemble(cfg: DictConfig) -> None:
-    """Train an ensemble of dynamics models."""
-
-    # prepare
-    # os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg["cuda_device"])
-    with open_dict(cfg):
-        # set the name of each dim of the output
-        with open(os.path.join(cfg["data_path"], 'info.pkl'), 'rb') as f:
-            info = pickle.load(f)
-            cfg['model']['dim_name_map'] = info["state_space"]
-        # set other hyperparameters
-        # if "gpus" in cfg:
-        #     cfg["gpus"] = str(cfg["gpus"])
-
-    num_models = cfg.get("ensemble_size")
-    outer_loop_size = cfg.get("outer_loop_size")
-    ray_env_var = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    start_time = time.time()
-    within_bounds = 0.0
-    num_models_so_far = 0
-    np.random.seed(cfg.get("seed"))
-    for outer_iter in range(outer_loop_size):
-        if cfg.get("is_hulc"):
-            B_star = get_B_star(cfg.get("alpha"), cfg.get("hulc_bias")) # there is randomness here, so we also need to set random seed in this function
-            num_models = B_star
-        else:
-            B_star = -1 
-        # sequential version
-        # results = [] 
-        # for i in range(num_models):
-        #     print(f"\n=== Training model {i + 1}/{num_models} ===")
-        #     result = train_single_model(cfg, i, outer_iter, B_star, num_models_so_far)
-        #     results.append(result)
-        # parallel version
-        ray.init(runtime_env={"env_vars": {"PYTHONPATH": ray_env_var}}, log_to_driver=False)
-        results = ray.get([train_single_model.remote(cfg, i, outer_iter, B_star, num_models_so_far) for i in range(num_models)])
-        ray.shutdown() # TODO: remove this and put ray.init outside to improve speed?
-
-        # collect/analyze training results
-        num_models_so_far += num_models
-        metrics = []
-        for i in range(num_models):
-            metrics.append(results[i][0])
-            if results[i][1] is not None: 
-                if outer_iter == 0: # since the test shots and gt_model are shared among all repeated experiments
-                    test_shots = results[i][1]
-                    gt_EYX = get_EYX(results[i][1], cfg["gt_model_path"])
-                learned_ensemble_dir = results[i][2]
-        
-        # get ensemble predictions on the test dataset
-        ensemble_EYX = get_EYX(test_shots, learned_ensemble_dir, ensemble_mode=True)
-        # check if gt_EYX falls within the 95% quantile interval
-        if not cfg.get("is_cheap"):
-            if not cfg.get("is_hulc"):
-                percentage = cfg.get("alpha") * 100.0
-                lower_bound = np.percentile(ensemble_EYX, percentage / 2.0, axis=0) 
-                upper_bound = np.percentile(ensemble_EYX, 100.0 - percentage / 2.0, axis=0)  
-            else:
-                lower_bound = np.min(ensemble_EYX, axis=0)
-                upper_bound = np.max(ensemble_EYX, axis=0)
-        else:
-            lower_bound, upper_bound = get_t_confidence_interval(ensemble_EYX, cfg.get("alpha"))
-        within_bounds += ((gt_EYX >= lower_bound) & (gt_EYX <= upper_bound)).astype(float)  # Shape: (batch_size, datapoint_dim)
-
-        print("=== Ensemble Training at Iteration {} Complete ===".format(outer_iter))
-        print("Test loss for each ensemble member:", metrics)
-        # get eta
-        elapsed_time = time.time() - start_time
-        remaining_iters = outer_loop_size - (outer_iter + 1)
-        eta = elapsed_time / (outer_iter + 1) * remaining_iters
-        print("ETA: {} hours".format(eta/3600.0))
-        # check within_bounds
-        average_within_bounds = within_bounds / float(outer_iter+1)
-        print("Average within_bounds ratio:", average_within_bounds.mean())
-        print("Average within_bounds ratio at each dimension:", average_within_bounds.mean(axis=0))
-        print("\n")
+        return 0
 
 
 if __name__ == "__main__":
-    train_ensemble()
+    train()
